a09e89e988b00980ea9a010ae37015c94f7c76e9
Jobson Barbosa <jobsonbarbosa86@gmail.com>
2024-11-25 20:53:52 -0300

    Criando tela de login e validando senha de acesso, Atualizando base de dados com usário e senha

--- SQLALCHEMY_CRUD/BANCO_USUARIOS/app.py
+++ SQLALCHEMY_CRUD/BANCO_USUARIOS/app.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from werkzeug.security import generate_password_hash, check_password_hash
 
 from sqlalchemy import create_engine, String, Boolean, select
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, Session
@@ -14,12 +15,18 @@ class Usuario(Base):
     
     id: Mapped[int] = mapped_column(primary_key=True)
     nome: Mapped[str] = mapped_column(String(30))
-    senha: Mapped[str] = mapped_column(String(30))
+    senha: Mapped[str] = mapped_column(String(128))
     email: Mapped[str] = mapped_column(String(30))
     acesso_gestor: Mapped[bool] = mapped_column(Boolean(), default=False)
     
     def __repr__(self):
         return f"Usuário({self.id=}, {self.nome=})"
+    
+    def define_senha(self, senha):
+        self.senha = generate_password_hash(senha)
+    
+    def verifica_senha(self, senha):
+        return check_password_hash(self.senha, senha)
 
 engine = create_engine(f'sqlite:///{PATH_TO_BD}')
 Base.metadata.create_all(bind=engine)
@@ -35,10 +42,10 @@ def cria_usuario(
     with Session(bind=engine) as session:
         usuario = Usuario(
             nome=nome,
-            senha=senha,
             email=email,
             acesso_gestor=acesso_gestor
         )
+        usuario.define_senha(senha)
         session.add(usuario)
         session.commit()
 
@@ -54,14 +61,50 @@ def le_usuario_por_id(id):
         comando_sql = select(Usuario).filter_by(id=id)
         usuarios = session.execute(comando_sql).fetchall()
         return usuarios[0][0]
+    
+def modifica_usuario(
+        id, 
+        **kwargs
+        # nome=None,
+        # senha=None,
+        # email=None,
+        # acesso_gestor=None
+        ):
+    with Session(bind=engine) as session:
+        comando_sql = select(Usuario).filter_by(id=id)
+        usuarios = session.execute(comando_sql).fetchall()
+        for usuario in usuarios:  
+            for key, value in kwargs.items():
+                if key == 'senha':
+                    usuario[0].define_senha(value)
+                else:
+                    setattr(usuario[0], key, value)
+            # if nome:
+            #     usuario[0].nome = nome
+            # if senha:
+            #     usuario[0].senha = senha
+            # if email:
+            #     usuario[0].email = email
+            # if not acesso_gestor is None:
+            #     usuario[0].acesso_gestor = acesso_gestor
+        session.commit()
+    
+def deleta_usuario(id):
+    with Session(bind=engine) as session:
+        comando_sql = select(Usuario).filter_by(id=id)
+        usuarios = session.execute(comando_sql).fetchall()
+        for usuario in usuarios:
+            session.delete(usuario[0])
+        session.commit()
 
 if __name__ == '__main__':
-#     cria_usuario(
-#         nome='Ana Carolina de Jesus Barbosa',
-#         senha='123456',
-#         email='janacarolinadejesusbarbosa@gmail.com',
-#         acesso_gestor=False,
-#)
+    
+    cria_usuario(
+        nome='Ana Carolina Barbosa',
+        senha='senhafacilsempre',
+        email='anacarolinadejesusbarbosa@gmail.com',
+        acesso_gestor=False,
+)
     # print(le_todos_usuarios())
     
     # usuarios = le_todos_usuarios()
@@ -69,8 +112,18 @@ if __name__ == '__main__':
     # print(usuario_0)
     # print(usuario_0.nome, usuario_0.senha, usuario_0.email)
 
+    # usuario_jobson = le_usuario_por_id(id=1)
+    # print(usuario_jobson)
+    # print(usuario_jobson.nome, usuario_jobson.senha, usuario_jobson.email)
+    
+    # modifica_usuario(id=1, email='novojobsonbarbosa86@outlook.com.br')
+    
+    # deleta_usuario(id=2)
+
+    
+    
+    
+    # ===== verifica senha ====
+    
     usuario_jobson = le_usuario_por_id(id=1)
-    print(usuario_jobson)
-    print(usuario_jobson.nome, usuario_jobson.senha, usuario_jobson.email)
-    
-    
+    print(usuario_jobson.verifica_senha('1234567'))
