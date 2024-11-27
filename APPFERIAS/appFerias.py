8dc752c8b7d6f174b72e6aa421353e4c8b9421b7
Jobson Barbosa <jobsonbarbosa86@gmail.com>
2024-11-27 17:25:24 -0300

    Criação da pagina de login e função de autenticação no banco de dados

--- APPFERIAS/appFerias.py
+++ APPFERIAS/appFerias.py
@@ -1,120 +1,45 @@
-from pathlib import Path
-from werkzeug.security import generate_password_hash, check_password_hash
+from time import sleep
+import streamlit as st
+from crud import le_todos_usuarios
 
-from sqlalchemy import create_engine, String, Boolean, Integer, select, ForeignKey
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, Session, relationship
 
-pasta_atual = Path(__file__).parent
-PATH_TO_BD = pasta_atual / 'bd_usuarios.sqlite'
-
-class Base(DeclarativeBase):
-    pass
-
-# Tabela de Usuários
-class UsuarioFerias(Base):
-    __tablename__ = 'usuarios_ferias'
+def login():
+    usuarios = le_todos_usuarios()
+    #st.write(usuarios) # Isso é uma lista e vamos transforma-la em um dicionário.
+    usuarios = {usuario.nome: usuario for usuario in usuarios}
+    #st.write(usuarios)
+    with st.container(border=True):
+        st.markdown('Bem vindo ao AppFerias')
+        
+        nome_usuario = st.selectbox(
+            'Selecione o seu usuário',
+            usuarios.keys()
+            )
+        senha = st.text_input('Digite a sua senha', type='password')
+        
+        if st.button('Acessar'):
+            usuario = usuarios[nome_usuario]
+            if usuario.verifica_senha(senha):
+                st.success('Login com sucesso')
+                st.session_state['logado'] = True
+                st.session_state['usuario'] = usuario
+                sleep(1)
+                st.rerun()
+            else:
+                st.error('Senha incorreta')
     
-    id: Mapped[int] = mapped_column(primary_key=True)
-    nome: Mapped[str] = mapped_column(String(30))
-    senha: Mapped[str] = mapped_column(String(128))
-    email: Mapped[str] = mapped_column(String(30))
-    acesso_gestor: Mapped[bool] = mapped_column(Boolean(), default=False)
-    inicio_na_empresa: Mapped[str] = mapped_column(String(30))
-    eventos_ferias: Mapped[list['EventosFerias']] = relationship(
-        back_populates='parent',
-        lazy='subquery'
-    )
+def pagina_calendario():
+    st.markdown('Pagina calendario')
+
+def main():
     
-    def __repr__(self):
-        return f"Usuário({self.id=}, {self.nome=})"
+    if not 'logado' in st.session_state:
+        st.session_state['logado'] = False
+        
+    if not st.session_state['logado']:
+        login()
+    else:
+        pagina_calendario()
     
-    def define_senha(self, senha):
-        self.senha = generate_password_hash(senha)
-    
-    def verifica_senha(self, senha):
-        return check_password_hash(self.senha, senha)
-
-# Tabela de Ferias  
-class EventosFerias(Base):
-    __tablename__ = 'eventos_ferias'
-    id: Mapped[int] = mapped_column(primary_key=True)
-    parent_id: Mapped[int] = mapped_column(ForeignKey('usuarios_ferias.id')) # referencia o usuário da tabela UsuarioFerias
-    parent: Mapped['UsuarioFerias'] = relationship(lazy='subquery') # Deixa explicito que há uma relação entre tabelas
-    inicio_ferias: Mapped[str] =  mapped_column(String(30))
-    fim_ferias: Mapped[str] = mapped_column(String(30))
-    total_dias: Mapped[int] = mapped_column(Integer())
-    
-engine = create_engine(f'sqlite:///{PATH_TO_BD}')
-Base.metadata.create_all(bind=engine)
-
-
-# CRUD =============================== #
-
-def cria_usuario(
-    nome,
-    senha,
-    email,
-    acesso_gestor=False
-):
-    with Session(bind=engine) as session:
-        usuario = Usuario(
-            nome=nome,
-            email=email,
-            acesso_gestor=acesso_gestor
-        )
-        usuario.define_senha(senha)
-        session.add(usuario)
-        session.commit()
-
-def le_todos_usuarios():
-    with Session(bind=engine) as session:
-        comando_sql = select(Usuario)
-        usuarios = session.execute(comando_sql).fetchall()
-        usuarios = [user[0] for user in usuarios]
-        return usuarios
-
-def le_usuario_por_id(id):
-    with Session(bind=engine) as session:
-        comando_sql = select(Usuario).filter_by(id=id)
-        usuarios = session.execute(comando_sql).fetchall()
-        return usuarios[0][0]
-    
-def modifica_usuario(
-        id, 
-        **kwargs
-
-        ):
-    with Session(bind=engine) as session:
-        comando_sql = select(Usuario).filter_by(id=id)
-        usuarios = session.execute(comando_sql).fetchall()
-        for usuario in usuarios:  
-            for key, value in kwargs.items():
-                if key == 'senha':
-                    usuario[0].define_senha(value)
-                else:
-                    setattr(usuario[0], key, value)
-
-        session.commit()
-    
-def deleta_usuario(id):
-    with Session(bind=engine) as session:
-        comando_sql = select(Usuario).filter_by(id=id)
-        usuarios = session.execute(comando_sql).fetchall()
-        for usuario in usuarios:
-            session.delete(usuario[0])
-        session.commit()
-
 if __name__ == '__main__':
-    
-    cria_usuario(
-        nome='Ana Carolina Barbosa',
-        senha='senhafacilsempre',
-        email='anacarolinadejesusbarbosa@gmail.com',
-        acesso_gestor=False,
-    )
-
-    
-    # ===== verifica senha ====
-    
-    usuario_jobson = le_usuario_por_id(id=1)
-    print(usuario_jobson.verifica_senha('1234567'))
+    main()
---
+++ APPFERIAS/crud.py
@@ -0,0 +1,136 @@
+from pathlib import Path
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from sqlalchemy import create_engine, String, Boolean, Integer, select, ForeignKey
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, Session, relationship
+
+pasta_atual = Path(__file__).parent
+PATH_TO_BD = pasta_atual / 'bd_usuarios.sqlite'
+
+class Base(DeclarativeBase):
+    pass
+
+# Tabela de Usuários
+class UsuarioFerias(Base):
+    __tablename__ = 'usuarios_ferias'
+    
+    id: Mapped[int] = mapped_column(primary_key=True)
+    nome: Mapped[str] = mapped_column(String(30))
+    senha: Mapped[str] = mapped_column(String(128))
+    email: Mapped[str] = mapped_column(String(30))
+    acesso_gestor: Mapped[bool] = mapped_column(Boolean(), default=False)
+    inicio_na_empresa: Mapped[str] = mapped_column(String(30))
+    eventos_ferias: Mapped[list['EventosFerias']] = relationship(
+        back_populates='parent',
+        lazy='subquery'
+    )
+    
+    def __repr__(self):
+        return f"Usuário({self.id=}, {self.nome=})"
+    
+    def define_senha(self, senha):
+        self.senha = generate_password_hash(senha)
+    
+    def verifica_senha(self, senha):
+        return check_password_hash(self.senha, senha)
+
+# Tabela de Ferias  
+class EventosFerias(Base):
+    __tablename__ = 'eventos_ferias'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    parent_id: Mapped[int] = mapped_column(ForeignKey('usuarios_ferias.id')) # referencia o usuário da tabela UsuarioFerias
+    parent: Mapped['UsuarioFerias'] = relationship(lazy='subquery') # Deixa explicito que há uma relação entre tabelas
+    inicio_ferias: Mapped[str] =  mapped_column(String(30))
+    fim_ferias: Mapped[str] = mapped_column(String(30))
+    total_dias: Mapped[int] = mapped_column(Integer())
+    
+engine = create_engine(f'sqlite:///{PATH_TO_BD}')
+Base.metadata.create_all(bind=engine)
+
+
+# CRUD =============================== #
+
+def cria_usuario(
+    nome,
+    senha,
+    email,
+    **kwargs
+):
+    with Session(bind=engine) as session:
+        usuario = UsuarioFerias(
+            nome=nome,
+            email=email,
+            **kwargs
+        )
+        usuario.define_senha(senha)
+        session.add(usuario)
+        session.commit()
+
+def le_todos_usuarios():
+    with Session(bind=engine) as session:
+        comando_sql = select(UsuarioFerias)
+        usuarios = session.execute(comando_sql).fetchall()
+        usuarios = [user[0] for user in usuarios]
+        return usuarios
+
+def le_usuario_por_id(id):
+    with Session(bind=engine) as session:
+        comando_sql = select(UsuarioFerias).filter_by(id=id)
+        usuarios = session.execute(comando_sql).fetchall()
+        return usuarios[0][0]
+    
+def modifica_usuario(
+        id, 
+        **kwargs
+
+        ):
+    with Session(bind=engine) as session:
+        comando_sql = select(UsuarioFerias).filter_by(id=id)
+        usuarios = session.execute(comando_sql).fetchall()
+        for usuario in usuarios:  
+            for key, value in kwargs.items():
+                if key == 'senha':
+                    usuario[0].define_senha(value)
+                else:
+                    setattr(usuario[0], key, value)
+
+        session.commit()
+    
+def deleta_usuario(id):
+    with Session(bind=engine) as session:
+        comando_sql = select(UsuarioFerias).filter_by(id=id)
+        usuarios = session.execute(comando_sql).fetchall()
+        for usuario in usuarios:
+            session.delete(usuario[0])
+        session.commit()
+
+if __name__ == '__main__':
+    pass
+    
+#     cria_usuario(
+#     'Jobson Barbosa',
+#     senha='123456',
+#     inicio_na_empresa = '2024-11-01',
+#     email='jobsonbarbosa86@gmail.com',
+#     acesso_gestor=True,
+# )
+#     cria_usuario(
+#         'Ana Carolina Barbosa',
+#         senha='123456',
+#         inicio_na_empresa = '2024-11-10',
+#         email='anacarolinadejesusbarbosa@gmail.com',
+#         acesso_gestor=False,
+# )
+#     cria_usuario(
+#         'Joelma Almeida',
+#         senha='123456',
+#         inicio_na_empresa = '2024-11-20',
+#         email='joelmapalmito@gmail.com',
+#         acesso_gestor=False,
+# )
+
+    
+    # ===== verifica senha ====
+    
+# usuario_jobson = le_usuario_por_id(id=1)
+# print(usuario_jobson.verifica_senha('1234567'))
