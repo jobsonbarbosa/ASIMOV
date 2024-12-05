32f4a97664004ede918f6d41627cdca690814994
Jobson Barbosa <jobsonbarbosa86@gmail.com>
2024-12-05 13:20:30 -0300

    criação do chat e input de usuário

--- ORACULO_IA/app.py
+++ ORACULO_IA/app.py
@@ -1,9 +1,29 @@
 import streamlit as st
 
+
+
+MENSAGEM_EXEMPLO = [
+    ('user', "Olá Jobs"),
+    ("assistant", "Tudo bem!?"),
+    ("user", "Tudo ótimo!")
+]
+
 def pagina_chat():
     st.header("Seja bem vindo ao Jobs Oracle", divider=True)
+    
+    # Guarda as messangem na sessão, se não houver retornar uma dicionário vazio.
+    mensagens = st.session_state.get('mensagens', MENSAGEM_EXEMPLO)
+    
+    for mensagem in mensagens:
+        chat = st.chat_message(mensagem[0])
+        chat.markdown(mensagem[1])
 
-    menssagens = st.session_state( 'mensagens')
+    input_usuario = st.chat_input('Fale com o Oráculo')
+    if input_usuario:
+        mensagens.append(("user", input_usuario))
+        
+        st.session_state['mensagens'] = mensagens
+        st.rerun()
 
 def main():
     pagina_chat()
