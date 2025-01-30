6572ec87f073b4f543a172893e294d4b732306e1
Jobson Barbosa <jobsonbarbosa86@gmail.com>
2025-01-29 21:55:18 -0300

    Manipulando caminhos

--- LENDO_ESCREVENDO_ARQUIVOS/4_caminho_relativo_absoluto.py
+++ LENDO_ESCREVENDO_ARQUIVOS/4_caminho_relativo_absoluto.py
@@ -5,4 +5,5 @@ from pathlib import Path
 print(Path("primeira_pasta").exists()) #True
 
 # Saber onde o python esta sendo rodado
-print(Path.cwd())
+print(Path.cwd())  # C:\Python\ASIMOV\LENDO_ESCREVENDO_ARQUIVOS
+
