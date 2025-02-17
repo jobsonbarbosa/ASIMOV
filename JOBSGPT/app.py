95d77726ac93bb047a9b459a9494f3c478480f72
Jobson Barbosa <jobsonbarbosa86@gmail.com>
2025-02-16 22:40:33 -0300

    criando a pagina de conversa, selecionando um arquivos, selecionando conversas salvas

---
+++ JOBSGPT/app.py
@@ -0,0 +1,175 @@
+import streamlit as st
+import openai
+from dotenv import load_dotenv
+import os
+from unidecode import unidecode
+import re
+from pathlib import Path
+import pickle
+
+# Carregar variáveis de ambiente
+load_dotenv()
+
+# Caminho da pasta
+PASTA_MENSAGENS = Path(__file__).parent / 'mensagens'
+PASTA_MENSAGENS.mkdir(exist_ok=True)
+CACHE_DESCONVERTE = {}
+
+
+# Configurar a chave da OpenAI
+api_key = os.getenv("OPENAI_API_KEY")
+openai.api_key = api_key
+
+def retorna_nome_da_mensagem(mensagens):
+    nome_mensagem = ''
+    for mensagem in mensagens:
+        if mensagem['role'] == 'user':
+            nome_mensagem = mensagem['content'][:30]
+            break
+    return nome_mensagem
+
+# Função para obter resposta do modelo OpenAI
+def retorna_resposta_modelo(mensagens,
+                            openai_key,
+                            modelo='gpt-3.5-turbo',
+                            temperatura=0,
+                            stream=False):
+    openai.api_key = openai_key
+    response = openai.ChatCompletion.create(
+        model=modelo,
+        messages=mensagens,
+        temperature=temperatura,
+        stream=stream
+    )
+    return response
+
+def converte_nome_mensagem(nome_mensagem):
+
+        # Removendo acentos
+    nome_arquivo = unidecode(nome_mensagem)
+
+    # Removendo os caracteres especiais
+    nome_arquivo = re.sub('\W+', '', nome_arquivo).lower()
+    return nome_arquivo
+
+def desconverte_nome_mensagem(nome_arquivo, key='mensagem'):
+    if not nome_arquivo in CACHE_DESCONVERTE:
+        nome_mensagem = ler_mensagem_por_nome_arquivo(nome_arquivo, key='nome_mensagem')
+        CACHE_DESCONVERTE[nome_arquivo] = nome_mensagem
+        return CACHE_DESCONVERTE[nome_arquivo]
+
+def salvar_mensagens(mensagens):
+    # Validando se há mensagens
+    if len(mensagens) == 0:
+        return False
+    
+    nome_mensagem = retorna_nome_da_mensagem(mensagens)
+    nome_arquivo = converte_nome_mensagem(nome_mensagem)
+    arquivo_salvar = {'nome_mensagem': nome_mensagem, 'nome_arquivo': nome_arquivo, 'mensagem': mensagens}
+    
+    # Salvando arquivos
+    with open(PASTA_MENSAGENS / nome_arquivo,'wb') as f:
+        pickle.dump(arquivo_salvar, f)
+
+def ler_mensagem_por_nome_arquivo(nome_arquivo, key='mensagem'):
+    with open(PASTA_MENSAGENS / nome_arquivo,'rb') as f:
+        mensagens = pickle.load(f)
+    return mensagens[key]
+
+#Lenddo mensagens
+def ler_mensagem(mensagens, key='mensagem'):
+    if len(mensagens) == 0:
+        return []
+    nome_mensagem = retorna_nome_da_mensagem(mensagens)
+    nome_arquivo = converte_nome_mensagem(nome_mensagem)
+    with open(PASTA_MENSAGENS / nome_arquivo,'rb') as f:
+        mensagens = pickle.load(f)
+    return mensagens[key]
+
+def lista_conversas():
+    conversas = list(PASTA_MENSAGENS.glob('*'))
+    conversas = sorted(conversas, key=lambda item: item.stat().st_mtime_ns, reverse=True)
+    return [c.stem for c in conversas]
+                     
+def inicializacao():
+     # Inicializar histórico de mensagens no session_state
+    if not 'mensagens' in st.session_state:
+        st.session_state.mensagens = []
+    if not 'conversa_atual' in st.session_state:
+        st.session_state.conversa_atual = ''
+
+# Função principal da página
+def pagina_principal():
+
+    
+    mensagens = ler_mensagem(st.session_state['mensagens'], key='mensagem')
+
+    st.header('🤖 Jobs Chatbot', divider=True)
+
+    # Exibir mensagens anteriores
+    for mensagem in mensagens:
+        chat = st.chat_message(mensagem['role'])
+        chat.markdown(mensagem['content'])
+    
+    # Capturar entrada do usuário
+    prompt = st.chat_input('Fale com o chat')
+    if prompt:
+        nova_mensagem = {'role': 'user',
+                         'content': prompt}
+        chat = st.chat_message(nova_mensagem['role'])
+        chat.markdown(nova_mensagem['content'])
+        mensagens.append(nova_mensagem)
+
+        # Exibir a mensagem do usuário
+        chat = st.chat_message('assistant')
+        placeholder = chat.empty()
+        placeholder.markdown("▌")
+        resposta_completa = ''
+        respostas = retorna_resposta_modelo(mensagens,
+                                            api_key,    
+                                            stream=True)
+
+        # Exibir resposta do chatbot
+        for resposta in respostas:
+            resposta_completa += resposta.choices[0].delta.get('content', '')
+            placeholder.markdown(resposta_completa + "▌")
+        placeholder.markdown(resposta_completa)
+        nova_mensagem = {'role': 'assistant',
+                         'content': resposta_completa}
+        
+        # Adicionar a mensagem ao histórico
+        mensagens.append(nova_mensagem)
+
+
+        st.session_state['mensagens'] = mensagens
+        salvar_mensagens(mensagens)
+
+def tab_conversas(tab):
+    tab.button('➕ Nova conversa', on_click=seleciona_conversa, args=('', ), use_container_width=True)
+
+    tab.markdown('')
+    conversas = lista_conversas()
+    for nome_arquivo in conversas:
+        nome_mensagem = desconverte_nome_mensagem(nome_arquivo).capitalize()
+        if len(nome_mensagem) == 30:
+            nome_arquivo += '...'
+        tab.button(nome_mensagem, on_click=seleciona_conversa, args=(nome_arquivo,), disabled=nome_arquivo==st.session_state['conversa_atual'], use_container_width=True)
+
+def seleciona_conversa(nome_arquivo):
+    if nome_arquivo == '':
+        st.session_state.mensagens = []
+    else:
+        mensagem = ler_mensagem_por_nome_arquivo(nome_arquivo, key='mensagem')
+        st.session_state.mensagens = mensagem
+        st.session_state['conversa_atual'] = nome_arquivo
+
+def main():
+
+    inicializacao()
+    pagina_principal()
+
+    tab1, tab2 = st.sidebar.tabs(['Conversas', 'Configurações'])
+    tab_conversas(tab1)
+
+if __name__ == '__main__':
+    main()
